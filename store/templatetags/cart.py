d2b705bdda965c99cee8b73348c48f6ec3b1b123
ducanh501 <lbducanh140401@gmail.com>
2022-11-16 15:53:58 +0700

    deploy web vegefoods

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -7,9 +7,9 @@ register = template.Library ()
 def is_in_cart(product, cart):
     keys = cart.keys ()
     for id in keys:
-        if int (id) == product.id:
+        if int(id) == product.id:
             return True
-    return False;
+    return False
 
 
 @register.filter (name='cart_quantity')
@@ -18,7 +18,7 @@ def cart_quantity(product, cart):
     for id in keys:
         if int (id) == product.id:
             return cart.get (id)
-    return 0;
+    return 0
 
 
 @register.filter (name='price_total')
@@ -28,7 +28,7 @@ def price_total(product, cart):
 
 @register.filter (name='total_cart_price')
 def total_cart_price(products, cart):
-    sum = 0;
+    sum = 0
     for p in products:
         sum += price_total (p, cart)
 
